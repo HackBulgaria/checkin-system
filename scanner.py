ec0400cf45024c8654e65da108c522cad0bc9926
root <bachvarof@gmail.com>
2015-05-02 12:03:49 +0300

    Add the scaner.py program

---
+++ scanner.py
@@ -0,0 +1,57 @@
+from subprocess import check_output
+
+import urllib 
+import urllib2
+import time
+import re
+import sys
+import os
+
+def send_code(mac):
+    url = 'https://hackbulgaria.com/set-check-in/'
+    values = {
+        'mac' : mac,
+        'token' : get_token(),
+    }
+    try:
+        data = urllib.urlencode(values)
+        req = urllib2.Request(url, data)
+        response = urllib2.urlopen(req, timeout=4)
+
+        if response.getcode() == 200:
+            print ("checked successfully")
+        if response.getcode() == 404:
+            print (mac + " user not found")
+
+    except Exception, detail:
+        print detail
+        print values
+
+
+def get_macs():
+    print("Hello I will check for macs now!")
+    result = check_output(["nmap", "-sP", "192.168.1.0/24"])
+    #No no not do it this way!
+    macs_alive = re.findall(r'[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}', result)
+    
+    for mac in macs_alive:
+        send_code(mac)
+        time.sleep(2)
+
+
+def get_token():
+    key_file_name = '/root/bin/unique.key'
+
+    if os.path.isfile(key_file_name):
+        hash_code = tuple(open(key_file_name, 'r'))[0]
+    else:
+        hash_code = os.urandom(16).encode('hex')
+        with open(key_file_name, 'w') as key_file: key_file.write(hash_code)
+    
+    return hash_code
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == '--get-token':
+	print(get_token())
+    else:
+        get_macs()
